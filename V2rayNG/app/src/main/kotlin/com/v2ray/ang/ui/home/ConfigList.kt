82698cfff806f1c742f97bddc58a32f710dbc360
Hadi Norouzi <hadinorouzi78@gmail.com>
2023-09-18 02:47:59 +0330

    add more item in compose widgets

--- V2rayNG/app/src/main/kotlin/com/v2ray/ang/ui/home/ConfigList.kt
+++ V2rayNG/app/src/main/kotlin/com/v2ray/ang/ui/home/ConfigList.kt
@@ -1,18 +1,38 @@
 package com.v2ray.ang.ui.home
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Delete
+import androidx.compose.material.icons.outlined.Edit
+import androidx.compose.material.icons.outlined.Share
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.v2ray.ang.dto.ServerConfig
 
 @Composable
 fun ConfigList(configs: List<ServerConfig>) {
 
-    LazyColumn {
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = Modifier.padding(8.dp),
+        userScrollEnabled = true
+    ) {
 
-        items(configs) {
-            Text(it.remarks)
+        items(configs + configs + configs + configs) {
+            ConfigItem(item = it)
         }
     }
 }
+
