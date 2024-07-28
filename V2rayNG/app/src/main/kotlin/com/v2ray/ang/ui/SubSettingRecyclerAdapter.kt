bdc4426241f39282be3e7af0371197745947d3ba
Hadi Norouzi <h.noroozi@ernyka.com>
2024-07-28 17:14:37 +0330

    Fix sub share button

--- V2rayNG/app/src/main/kotlin/com/v2ray/ang/ui/SubSettingRecyclerAdapter.kt
+++ V2rayNG/app/src/main/kotlin/com/v2ray/ang/ui/SubSettingRecyclerAdapter.kt
@@ -3,8 +3,8 @@ package com.v2ray.ang.ui
 import android.graphics.Color
 import android.text.TextUtils
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.v2ray.ang.R
 import com.v2ray.ang.databinding.ItemRecyclerSubSettingBinding
@@ -27,22 +27,24 @@ class SubSettingRecyclerAdapter(
     private var mActivity: SubSettingActivity = activity
 
 
-
     override fun getItemCount() = mActivity.subscriptions.size
 
     override fun onBindViewHolder(holder: SubscriptionViewHolder, position: Int) {
         val subId = mActivity.subscriptions[position].first
         val subItem = mActivity.subscriptions[position].second
-        holder.binding.tvName.text = subItem.remarks
-        holder.binding.tvUrl.text = subItem.url
-        if (subItem.enabled) {
-            holder.binding.chkEnable.setBackgroundResource(R.color.colorAccent)
-        } else {
-            holder.binding.chkEnable.setBackgroundResource(0)
-        }
+
         holder.itemView.setBackgroundColor(Color.TRANSPARENT)
 
         holder.binding.apply {
+
+            tvName.text = subItem.remarks
+            tvUrl.text = subItem.url
+            if (subItem.enabled) {
+                chkEnable.setBackgroundResource(R.color.colorAccent)
+            } else {
+                chkEnable.setBackgroundResource(0)
+            }
+
             layoutEdit.setOnClickListener {
                 actions?.onEditClick(subId)
             }
@@ -51,12 +53,10 @@ class SubSettingRecyclerAdapter(
                 notifyItemChanged(position)
             }
 
-            if (TextUtils.isEmpty(subItem.url)) {
-                holder.binding.layoutShare.visibility = View.INVISIBLE
-            } else {
-                layoutShare.setOnClickListener {
-                    actions?.onShareClick(subItem.url)
-                }
+            layoutShare.isVisible = TextUtils.isEmpty(subItem.url).not()
+
+            layoutShare.setOnClickListener {
+                actions?.onShareClick(subItem.url)
             }
         }
 
